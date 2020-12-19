26bde9910e2c34d2752825d1232d53195a7c2bb2
Alena Dikovitska <alena.dikovitska@gmail.com>
2020-12-19 12:34:20 +0300

    hw 5: Coroutines

--- app/src/main/java/com/example/fundamentalskotlin/ActorsAdapterDiffUtil.kt
+++ app/src/main/java/com/example/fundamentalskotlin/ActorsAdapterDiffUtil.kt
@@ -1,5 +1,6 @@
 package com.example.fundamentalskotlin
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,26 +13,38 @@ import com.example.fundamentalskotlin.data.Actor
 
 class ActorsAdapterDiffUtil : RecyclerView.Adapter<ActorViewHolder>() {
 
-    private var actors: List<Actor> = listOf()
+    private var actorsList = listOf<Actor>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
-        val view: View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_holder_actor, parent, false)
-        return ActorViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder =
+        ActorViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_holder_actor, parent, false))
 
     override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
-        holder.onBind(actors[position])
+        holder.onBind(actorsList[position])
     }
 
-    override fun getItemCount(): Int = actors.size
+    override fun getItemCount(): Int = actorsList.size
 
     fun bindActors(newActors: List<Actor>) {
-        actors = newActors
+        actorsList = newActors
+        notifyDataSetChanged()
     }
 }
 
 class ActorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val avatar: ImageView = itemView.findViewById(R.id.iv_actor_avatar)
+    private val name: TextView = itemView.findViewById(R.id.tv_actor_name)
+
+    fun onBind(actor: Actor) {
+        Glide.with(itemView.context)
+            .load(actor.picture)
+            .apply(imageOption)
+            .into(avatar)
+
+        name.text = actor.name
+        Log.d("Parcel", "actor.name = ${actor.name}")
+    }
 
     companion object {
         private val imageOption = RequestOptions()
@@ -39,18 +52,5 @@ class ActorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             .fallback(R.drawable.ic_avatar_placeholder)
             .centerCrop()
     }
-
-    private val avatar: ImageView = itemView.findViewById(R.id.iv_actor_avatar)
-    private val nameActor: TextView = itemView.findViewById(R.id.tv_actor_name)
-
-    fun onBind(actor: Actor) {
-        Glide.with(context)
-            .load(actor.avatar)
-            .apply(imageOption)
-            .into(avatar)
-        nameActor.text = actor.nameActor
-    }
 }
 
-private val RecyclerView.ViewHolder.context
-    get() = this.itemView.context
--- app/src/main/java/com/example/fundamentalskotlin/data/Movie.kt
+++ app/src/main/java/com/example/fundamentalskotlin/data/Movie.kt
@@ -1,57 +1,9 @@
 package com.example.fundamentalskotlin.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-
-data class Movie(
-    val image: String,
-    val ageRating: String,
-    val reviews: String,
-    val name: String,
-    val minut: String,
-    val genre: String,
-    val story: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(image)
-        parcel.writeString(ageRating)
-        parcel.writeString(reviews)
-        parcel.writeString(name)
-        parcel.writeString(minut)
-        parcel.writeString(genre)
-        parcel.writeString(story)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
-
-/*
-
+@Parcelize
 data class Movie(
     val id: Int,
     val title: String,
@@ -61,10 +13,10 @@ data class Movie(
     val ratings: Float,
     val adult: Boolean,
     val runtime: Int,
+    val reviews: Int,
     val genres: List<Genre>,
-    val actors: List<Actor>
-)
-
- */
+    val actors: List<Actor>,
+    val like: Boolean = false
+) : Parcelable
 
 
